9c2a2775a34a2ea8e3a7e1ff90881cb4c8cf4e53
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2023-05-21 15:45:50 -0400

    Animate back navigation

--- src/App.tsx
+++ src/App.tsx
@@ -44,12 +44,18 @@ function Movies() {
 }
 
 function MovieItem({ movie }: { movie: Movie }) {
-  const [navigate, ref] =
-    useNavigateWithViewTransition<HTMLImageElement>("movie-image");
+  const [navigate, ref] = useNavigateWithViewTransition<HTMLImageElement>({
+    viewTransitionName: "movie-image",
+  });
 
   return (
     <button onClick={() => navigate(`/movie/${movie.id}`)}>
-      <img ref={ref} src={movie.thumbnailURL.large} alt={movie.title} />
+      <img
+        ref={ref}
+        data-id={movie.id}
+        src={movie.thumbnailURL.large}
+        alt={movie.title}
+      />
     </button>
   );
 }
@@ -57,15 +63,22 @@ function MovieItem({ movie }: { movie: Movie }) {
 function Movie() {
   const { movieId } = useParams();
   const movie = movies.find((movie) => movie.id === movieId)!;
+  const [navigate] = useNavigateWithViewTransition<HTMLElement>({
+    selector: `[data-id="${movieId}"]`,
+    viewTransitionName: "movie-image",
+  });
 
   return (
-    <figure>
-      <img
-        style={{ viewTransitionName: "movie-image" }}
-        src={movie.thumbnailURL.large}
-        alt={movie.title}
-      />
-    </figure>
+    <div className="full">
+      <button onClick={() => navigate("/")}>✕</button>
+      <figure>
+        <img
+          style={{ viewTransitionName: "movie-image" }}
+          src={movie.thumbnailURL.large}
+          alt={movie.title}
+        />
+      </figure>
+    </div>
   );
 }
 
@@ -81,21 +94,45 @@ function NoMatch() {
 }
 
 function useNavigateWithViewTransition<T extends HTMLElement = HTMLElement>(
-  viewTransitionName: string
+  params: { viewTransitionName?: string; selector?: string } = {}
 ) {
   const ref = React.useRef<T>(null);
 
   const navigate = useNavigate();
   const navigateWithViewTransition = React.useCallback(
-    (nextRoute: To) => {
-      if (document.startViewTransition && ref.current) {
-        ref.current.style.viewTransitionName = viewTransitionName;
+    async (nextRoute: string) => {
+      const isBackNavigation = nextRoute === "/";
+      if (isBackNavigation) {
+        document.documentElement.classList.add("back-navigation");
+      }
+      if (document.startViewTransition) {
+        let element: Element | null = null;
+        if (ref.current) {
+          ref.current.style.viewTransitionName =
+            params.viewTransitionName ?? "";
+        }
 
-        document.startViewTransition(() => {
+        const transition = document.startViewTransition(() => {
           ReactDOM.flushSync(() => {
             navigate(nextRoute);
           });
+          if (isBackNavigation && params.selector) {
+            element = document.querySelector(params.selector);
+            if (element) {
+              (element as HTMLElement).style.viewTransitionName =
+                params.viewTransitionName ?? "";
+            }
+          }
         });
+
+        try {
+          await transition.finished;
+        } finally {
+          document.documentElement.classList.remove("back-navigation");
+          if (element) {
+            (element as HTMLElement).style.viewTransitionName = "";
+          }
+        }
       } else {
         navigate(nextRoute);
       }
